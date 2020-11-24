c8f01bbf0850362cdaa79d2ed149e3e406c3c0b6
Arnold Dziudzik <arn.dziudzik@gmail.com>
2020-11-24 19:14:20 +0200

    First commit: project bare-bones

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+function App() {
+  return (
+    <div>
+      Hello Arnold!
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+export default App;
