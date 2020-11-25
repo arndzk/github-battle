27af5012e40467f0e90fcdd6469481fa462f03f2
Arnold Dziudzik <arn.dziudzik@gmail.com>
2020-11-25 18:02:24 +0200

    progress: languages nav - clicking a nav link will update state's selectedLanguage with that of said nav link

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
 export default class Popular extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedLanguage: 'All'
+        }
+
+        this.updateLanguage = this.updateLanguage.bind(this);
+    }
+    
+    updateLanguage(selectedLanguage) {
+        this.setState({
+            selectedLanguage
+        })
+    }
+
     render() {
         const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'Python', 'C#'];
         return (
             <ul className='flex-center'>
                 {languages.map((language) => (
                     <li key={language}>
-                        <button className='btn-clear nav-link'>
+                        <button 
+                        className='btn-clear nav-link'
+                        style={language === this.state.selectedLanguage ? {color: '#77dd77'} : null}
+                        onClick={() => this.updateLanguage(language)}>
                             {language}
                         </button>
                     </li>
